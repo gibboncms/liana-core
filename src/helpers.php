8f997f55593d300b8e144ab7fb74d5cedc4919b5
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-05-31 22:11:12 +0200

    Added elixir

--- src/helpers.php
+++ src/helpers.php
@@ -31,3 +31,28 @@ if ( ! function_exists('markdown'))
         return $parser->parse($markdown);
     }
 }
+
+if ( ! function_exists('elixir'))
+{
+    /**
+    * Get the path to a versioned Elixir file.
+    *
+    * @param  string  $file
+    * @return string
+    */
+    function elixir($file)
+    {
+        static $manifest = null;
+
+        if (is_null($manifest)) {
+            $manifest = json_decode(file_get_contents(
+                env('PUBLIC_PATH', app()->basePath()).'/public/build/rev-manifest.json'), true);
+        }
+
+        if (isset($manifest[$file])) {
+            return '/build/'.$manifest[$file];
+        }
+        
+        throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
+    }
+}
